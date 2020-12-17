5fe2b8371c7397902d2285379c877c065d2f6b41
sck1ddie <75625068+sck1ddie@users.noreply.github.com>
2020-12-17 05:01:12 +0300

    Add files via upload

---
+++ sck1ddie.asm
@@ -0,0 +1,18 @@
+global _start
+
+section .text:
+
+section .data:
+	msj: db 'sck1ddie ile assembly :)',10	;10 veya 0xA == \n
+	len equ $-msj
+
+_start:
+	mov eax,0x4		; sys_write fonksiyonu
+	mov ebx,0x1		; stdout <stdio.h>
+	mov ecx,msj		; ecx = msj
+	mov edx,len		; mesaj uzunlugu
+	int 128			; kerneli getir
+
+	mov eax,0x1		; sys_exit
+	mov ebx,0x0
+	int 128			; kerneli getir 128 veya 0x80
